a633c77ffa65b3e5d14cf86d68b7edc2f76a2ca0
장유진 <yj999900@gmail.com>
2024-06-17 00:43:39 +0900

    .

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,5 @@
-print(5)
+url = "http://naver.com"
+my_str = url.replace("http://", "")
+my_str = my_str[0 : my_str.index(".")]
+password = my_str[0:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print(f"{url}의 비밀번호는 {password}입니다.")
