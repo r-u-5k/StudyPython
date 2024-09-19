d2bdef9db72ed4d64b60a3c28acf572efb1f163d
Yujin <yj999900@gmail.com>
2024-09-19 13:22:36 +0900

    .

--- advanced/advanced_hw2.py
+++ advanced/advanced_hw2.py
@@ -1,37 +1,29 @@
-def find_nearest_city(current_city, unvisited, graph):
-    nearest_city = None
-    min_distance = float('inf')
-    for city in unvisited:
-        distance = graph[current_city][city]
-        if distance < min_distance:
-            min_distance = distance
-            nearest_city = city
-    return nearest_city
+import pandas as pd
+from itertools import permutations
+
+distances = pd.DataFrame({
+    'A': [0, 12, 10, 19, 8],
+    'B': [12, 0, 3, 7, 2],
+    'C': [10, 3, 0, 6, 20],
+    'D': [19, 7, 6, 0, 4],
+    'E': [8, 2, 20, 4, 0]
+}, index=['A', 'B', 'C', 'D', 'E'])
+
+all_routes = [['A'] + list(p) + ['A'] for p in permutations(['B', 'C', 'D', 'E'])]
 
 
-def tsp(graph):
-    unvisited = list(graph.keys())
-    path = ['A']
-    current_city = 'A'
-
-    while len(unvisited) > 1:  # 'A'는 이미 방문했으므로 1개 남을 때까지
-        unvisited.remove(current_city)
-        next_city = find_nearest_city(current_city, unvisited, graph)
-        path.append(next_city)
-        current_city = next_city
-
-    path.append('A')
-    return path
+def calculate_total_distance(route):
+    total_distance = 0
+    for i in range(len(route) - 1):
+        start_city = route[i]
+        end_city = route[i + 1]
+        distance = distances.loc[start_city, end_city]
+        total_distance += distance
+    return total_distance
 
 
-graph = {
-    'A': {'B': 12, 'C': 10, 'D': 19, 'E': 8},
-    'B': {'A': 12, 'C': 3, 'D': 7, 'E': 2},
-    'C': {'A': 10, 'B': 3, 'D': 6, 'E': 20},
-    'D': {'A': 19, 'B': 7, 'C': 6, 'E': 4},
-    'E': {'A': 8, 'B': 2, 'C': 20, 'D': 4},
-}
+shortest_route = min(all_routes, key=calculate_total_distance)
+shortest_distance = calculate_total_distance(shortest_route)
 
-shortest_path = tsp(graph)
-
-print(f"최단 경로: {' -> '.join(shortest_path)}")
+print(f"최단 경로: {' -> '.join(shortest_route)}")
+print(f"최단 거리: {shortest_distance}")
