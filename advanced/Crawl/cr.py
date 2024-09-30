836eeab91c1f3923e83c6a9946f7822290aa3d4d
Yujin <yj999900@gmail.com>
2024-09-30 16:49:01 +0900

    .

---
+++ advanced/Crawl/cr.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+pd.set_option('display_width', 5000)
+pd.set_option('display_max_width', 5000)
+pd.set_option('display_max_columns', 5000)
