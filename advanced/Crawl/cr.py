6eaf3b3b42561c2f146a68e966aa2f4874b61178
Yujin <yj999900@gmail.com>
2024-10-01 13:11:24 +0900

    .

--- advanced/Crawl/cr.py
+++ advanced/Crawl/cr.py
@@ -36,20 +36,64 @@ def driversetting(DownloadPath):
     return driver
 
 
-def gen(): # TargetDay.Farm.Method
-    DownloadPath = "C:/Users/yj999/Downloads"
-    driver = driversetting(DownloadPath)
+def gen(TargetDay, Farm):  # TargetDay.Farm.Method
+    driver = driversetting(pa.DownloadPath)
     driver.get(pa.HYOSUNG)
-    print('run website')
-
+    print('Run Website')
     time.sleep(pa.waitseconds)
 
     driver.find_element(By.XPATH, '//*[@id="Txt_1"]').send_keys('jarasolar')
     driver.find_element(By.XPATH, '//*[@id="Txt_2"]').send_keys('abcd1234')
     driver.find_element(By.XPATH, '//*[@id="imageField"]').click()
-    print('login')
+    print('Login')
+    time.sleep(pa.waitseconds)
+
+    driver.find_element(By.XPATH, '//*[@id="form1"]/div[4]/div[1]/div/ul[2]/a[5]/li').click()
+    print('Statistical Report')
+    time.sleep(pa.waitseconds)
+
+    driver.find_element(By.XPATH, '//*[@id="SrTop_cbo_plant"]/option[' + str(Farm) + ']').click()
+    print('Select Farm')
+    time.sleep(pa.waitseconds)
+
+    driver.find_element(By.XPATH, '//*[@id="txt_Calendar"]').clear()
+    time.sleep(pa.waitseconds)
+
+    driver.find_element(By.XPATH, '//*[@id="txt_Calendar"]').send_keys(TargetDay)
+    print('Put New Date')
+    time.sleep(pa.waitseconds)
+
+    driver.find_element(By.XPATH, '//*[@id="txt_Calendar"]').send_keys(Keys.ENTER)
+    print('Close The Calendar')
+    time.sleep(pa.waitseconds)
+
+    driver.find_element(By.XPATH, '//*[@id="submitbtn"]').click()
+    print('Search')
+    time.sleep(pa.waitseconds)
+
+    driver.find_element(By.XPATH, '//*[@id="exldownbtn"]').click()
+    print('Download')
+    time.sleep(pa.waitseconds)
+
+    start = time.time()
+    count = 0
+    output = "Fail"
+    while 1:
+        file = os.listdir(pa.DownloadPath)
+        if len(file) == 0:
+            count = count + 1
+            time.sleep(pa.waitseconds)
+            if count == 10:
+                break
+
+        elif len(file) == 1:
+            if file == 'TimeData_' + TargetDay + '.xls':
+                FileName = os.path.join(pa.DownloadPath, file)
+                Data = pd.read_csv(FileName)
 
     return []
 
 if __name__ == "__main__":
-    gen()
+    Farm = 1
+    TargetDay = '2024-09-30'
+    gen(TargetDay, Farm)
--- advanced/params.py
+++ advanced/params.py
@@ -1,5 +1,7 @@
 CHROME_DRIVER_PATH = "C:/Dev/chromedriver-win64"
 
-waitseconds = 2
+waitseconds = 1
 
 HYOSUNG = "https://hess.hyosung.com/HsMain/HM020.aspx"
+
+DownloadPath = "C:/Users/yj999/Downloads"
