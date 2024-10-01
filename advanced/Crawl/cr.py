c6eb94391a9b8b1869f40941ecdc587f3ebf4f77
Yujin <yj999900@gmail.com>
2024-10-01 20:30:19 +0900

    .

--- advanced/Crawl/cr.py
+++ advanced/Crawl/cr.py
@@ -1,8 +1,10 @@
+import datetime
 import os
 import sys
 import time
 import pandas as pd
 import numpy as np
+import psycopg2
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -10,11 +12,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 import params as pa
-
-
-# pd.set_option('display_width', 5000)
-# pd.set_option('display_max_width', 5000)
-# pd.set_option('display_max_columns', 5000)
 
 
 def driversetting(DownloadPath):
@@ -25,19 +22,31 @@ def driversetting(DownloadPath):
                                               "safebrowsing.for_trusted_sources_enabled": False,
                                               "safebrowsing.enabled": False})
 
-    # if 1:
-    #     options.add_argument("--headless")
+    # options.add_argument("--headless")
 
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-sha-usage")
 
-    driver = webdriver.Chrome(options=options)
+    service = Service()
+
+    driver = webdriver.Chrome(service=service, options=options)
     driver.implicitly_wait(pa.waitseconds)
 
     return driver
 
 
-def gen(TargetDay, Farm):  # TargetDay.Farm.Method
+def fileremover():
+    file_list = os.listdir(pa.DownloadPath)
+    for f in file_list:
+        if f == '.DS_Store':
+            continue
+        else:
+            FileName = os.path.join(pa.DownloadPath, f)
+            os.remove(FileName)
+    return []
+
+
+def GetGenData(TargetDay, Farm):  # TargetDay.Farm.Method
     driver = driversetting(pa.DownloadPath)
     driver.get(pa.HYOSUNG)
     print('Run Website')
@@ -76,26 +85,83 @@ def gen(TargetDay, Farm):  # TargetDay.Farm.Method
     print('Download')
     time.sleep(pa.waitseconds)
 
-    start = time.time()
+    PreNow = datetime.datetime.today()
+    Today = PreNow.strftime("%Y-%m-%d")
+
     count = 0
-    output = "Fail"
     while 1:
         file = os.listdir(pa.DownloadPath)
         if len(file) == 0:
-            count = count + 1
+            count += 1
             time.sleep(pa.waitseconds)
             if count == 10:
                 break
 
-        elif len(file) == 1:
-            if file == 'TimeData_' + TargetDay + '.xls':
-                FileName = os.path.join(pa.DownloadPath, file)
-                Data = pd.read_csv(FileName)
+        elif len(file) > 0:
+            TargetFile = 'HourData_' + TargetDay + '.xls'
 
-    return []
+            if TargetFile in file:
+                breaker = 0
+                for f in file:
+                    if f == TargetFile:
+                        FileName = os.path.join(pa.DownloadPath, f)
+                        breaker = 1
+                        break
+                if breaker == 1:
+                    break
+
+    Data = getuploader(FileName, Farm)
+    # fileremover()
+    driver.close()
+
+    return Data
+
+
+def getuploader(FilePath, Farm):
+    conn = psycopg2.connect(host=pa.host, dbname=pa.dbname, user=pa.user, password=pa.password, port=pa.port)
+    cur = conn.cursor()
+
+    Now = datetime.datetime.today() - datetime.timedelta(hours=3)
+
+    xls_pv_html = pd.read_html(FilePath)[0]
+
+    Result = pd.DataFrame(columns=['target', 'actual'])
+    Result.assign(target=xls_pv_html['시간', '시간'])
+    Result.assign(actual=xls_pv_html['생산량(kWh)', 'PV 발전량'])
+    Result.assign(actual=Result['actual'].round(0))
+    Result.index = range(0, len(Result))
+
+    RealGen = np.where(Result['target'] != 'Sum')
+    Result = Result.loc[RealGen[0], :]
+    Result.assign(target=pd.to_datetime(Result['target'], format='%Y-%m-%d %H:%M', utc=False).dt.tz_localize(None))
+    Result.assign(site_id=Farm)
+
+    for i in range(0, len(Result)):
+        Target = Result.loc[i, 'target']
+        SiteID = Result.loc[i, 'site_id']
+        Actual = Result.loc[i, 'actual']
+
+        if Target > Now:
+            continue
+
+        select_all_sql = f"select EXISTS(select * from solar " \
+                         f"where target = TIMESTAMP '{Target}' and site_id = {SiteID})"
+
+        cur.execute(select_all_sql)
+        Exists = cur.fetchone()[0]
+
+        if not Exists:
+            print("Upload: ", Target, Actual, SiteID)
+            query = f"insert into solar (target, actual, site_id) values (TIMESTAMP {Target}, {Actual}, {SiteID})"
+            cur.execute(query)
+
+        else:
+            print("Duplicated ", Target, Actual, SiteID)
+
+    return Result
 
 
 if __name__ == "__main__":
     Farm = 1
     TargetDay = '2024-09-30'
-    gen(TargetDay, Farm)
+    GetGenData(TargetDay, Farm)
--- advanced/params.py
+++ advanced/params.py
@@ -5,3 +5,11 @@ waitseconds = 1
 HYOSUNG = "https://hess.hyosung.com/HsMain/HM020.aspx"
 
 DownloadPath = "C:/Users/yj999/Downloads"
+
+user = "postgres"
+host = "localhost"
+dbname = "postgres"
+port = 5432
+password = "yj9928"
+
+rotation = 1
