6739e806175a15409704f372579064fe16a4aceb
Yujin <yj999900@gmail.com>
2024-10-01 23:43:44 +0900

    .

--- advanced/AdvDjango/AdvDjango/settings.py
+++ advanced/AdvDjango/AdvDjango/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "example",
+    "EE",
+    "CBV",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -47,7 +52,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "AdvDjango.urls"
 
