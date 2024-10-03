5eb338a6d6271e975d8edad3809d9144f27d3e74
Yujin <yj999900@gmail.com>
2024-10-03 14:50:04 +0900

    .

--- advanced/Predictor/machine.py
+++ advanced/Predictor/machine.py
@@ -7,7 +7,8 @@ import numpy as np
 import random
 
 from dotenv import load_dotenv
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
 
 StartPath = os.getcwd()
 os.chdir('..')
@@ -39,72 +40,70 @@ def weather_read():
     return Weather
 
 
-# def regression():
-#     if pa.Machine == "RF":
-#         Machine = RandomForestRegressor(
-#             n_estimators=pa.RF_Tree,
-#             criterion="squared_error",
-#             max_depth=pa.RF_Depth,
-#             min_samples_split=pa.RF_MinLeaf * 2,
-#             min_samples_leaf=pa.RF_MinLeaf,
-#             n_jobs=pa.RF_njob,
-#             max_features=pa.RFMF,  # 'sqrt' auto None 1.0
-#             max_samples=pa.max_samples,  # 1.0, # 0.5, # 0.5 None
-#             bootstrap=True,  # Must True
-#             random_state=random.randint(0, 1000),
-#             verbose=0,
-#         )
+def regression():
+    if pa.Machine == "RF":
+        machine = RandomForestRegressor(
+            n_estimators=pa.RF_Tree,
+            criterion="squared_error",
+            max_depth=pa.RF_Depth,
+            min_samples_split=pa.RF_MinLeaf * 2,
+            min_samples_leaf=pa.RF_MinLeaf,
+            n_jobs=pa.RF_njob,
+            max_features=pa.RFMF,  # 'sqrt' auto None 1.0
+            max_samples=pa.max_samples,  # 1.0, # 0.5, # 0.5 None
+            bootstrap=True,
+            random_state=random.randint(0, 1000),
+            verbose=0,
+        )
 
-    # elif pa.Machine == "GBM":
-    #     machine = GradientBoostingRegressor(
-    #         n_estimators=pa.GBM_Tree,
-    #         learning_rate=pa.GBM_LR,
-    #         max_depth=pa.GBM_Depth,
-    #         random_state=random.randint(0, 1000),
-    #         criterion="friedman_mse",
-    #         subsample=pa.subsample,
-    #         min_samples_split=2,
-    #         max_features=pa.GBMMF,  # sqrt auto
-    #         verbose=0,
-    #         loss="squared_error",
-    #     )
-    #
-    # elif pa.Machine == "LR":
-    #     machine = LinearRegression(fit_intercept=True)
-    #
-    # return Machine
+    elif pa.Machine == "GBM":
+        machine = GradientBoostingRegressor(
+            n_estimators=pa.GBM_Tree,
+            learning_rate=pa.GBM_LR,
+            max_depth=pa.GBM_Depth,
+            random_state=random.randint(0, 1000),
+            criterion="friedman_mse",
+            subsample=pa.subsample,
+            min_samples_split=2,
+            max_features=pa.GBMMF,  # sqrt auto
+            verbose=0,
+            loss="squared_error",
+        )
+
+    elif pa.Machine == "LR":
+        machine = LinearRegression(fit_intercept=True)
+
+    return machine
 
 
-# def training(TrainingData):
-#     Y_train = TrainingData["Target"].copy()
-#     X_train = TrainingData.copy()
-#     del X_train["Target"]
-#     del X_train["DeliveryDT"]
-#     del X_train["WeatherStationId"]
-#
-#     Machine = regression()
-#     Machine.fit(X_train, Y_train)
-#
-#     pred = Machine.predict(X_train)
-#     pred = np.round(pred, 2)
-#
-#     plt.figure(1)
-#     plt.plot(TrainingData['DeliveryDT'], Y_train, label="Actual")
-#     plt.plot(TrainingData['DeliveryDT'], pred, label="prediction")
-#     plt.legend()
-#     plt.grid(True)
-#     plt.draw()
-#     plt.interactive(False)
-#     # plt.show(block=False)
-#     plt.show()
-#
-#     return pred
+def training(TrainingData):
+    Y_train = TrainingData["Target"].copy()
+    X_train = TrainingData.copy()
+    del X_train["Target"]
+    del X_train["DeliveryDT"]
+    del X_train["WeatherStationId"]
+
+    Machine = regression()
+    Machine.fit(X_train, Y_train)
+
+    pred = Machine.predict(X_train)
+    pred = np.round(pred, 2)
+
+    plt.figure(1)
+    plt.plot(TrainingData['DeliveryDT'], Y_train, label="Actual")
+    plt.plot(TrainingData['DeliveryDT'], pred, label="Prediction")
+    plt.legend()
+    plt.grid(True)
+    plt.draw()
+    plt.interactive(False)
+    # plt.show(block=False)
+    plt.show()
+
+    return pred
 
 
 if __name__ == "__main__":
     Solar = solar_read()
     Weather = weather_read()
-    # TrainingData = pd.merge(Solar, Weather, how='inner', on='DeliveryDT')
-    # Output = training(TrainingData)
-    print(Solar)
-    print(Weather)
+    TrainingData = pd.merge(Solar, Weather, how='inner', on='DeliveryDT')
+    Output = training(TrainingData)
